c195a61a4b4571f70de430e67f5394a61057da00
Chris Larson <c_larson91@live.com>
2017-06-03 11:13:13 -0400

    Adds functionality to GET and POST endpoints

---
+++ README.md
@@ -0,0 +1,26 @@
+# Demeritron-Api
+
+This application serves as an API layer the Demeritron Bot (demeritron.herokuapp.com)
+The server utilizes a Neo4j graph database to maintain relationship data between users and tracks the number of demerits each has given to each other.
+
+## API
+Base URL: https://demeritron-api.herokuapp.com
+
+### Health
+Determine if the server is live and healthy
+`GET /health`
+Returns a 200 response if the server is live
+
+### Add Demerit
+Give a demerit from one user to another
+`POST /demerits`
+```
+{
+    "to": string,
+    "from": string
+}
+```
+
+### Fetch Data
+Returns all users and their relational demerit data
+`GET /demerits`
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-let port = process.env.PORT || 3000;
-let router = express.Router(); 
+const port = process.env.PORT || 3000;
+const router = express.Router(); 
+
+// Graph DB
+const dbUrl = process.env.GRAPHENEDB_URL;
+const neo4j = require('neo4j');
+const db = new neo4j.GraphDatabase(dbUrl);
 
 router.get('/health', function(req, res) {
     res.json({ message: 'server is healthy' });   
@@ -23,17 +28,202 @@ router.post('/demerits', function(req, res) {
     if (!req.body.to || !req.body.from) {
         res.status(400).json({message: "missing required field 'to' or 'from'"});
     } else {
-        // Do graph db stuff
-        res.json({ message: "done" });
+        const to = req.body.to.toLowerCase();
+        const from = req.body.from.toLowerCase();
+        try {
+            // Make Cypher requests to Db server
+            userExists(to).then((toUserExists) => {
+                if (!toUserExists) {
+                    createUser(to);
+                }
+            }).then(() => {
+                return userExists(from);
+            }).then((fromUserExists) => {
+                if (!fromUserExists) {
+                    createUser(from)
+                }
+            }).then(() => {
+                return relationshipExists(to, from);
+            }).then((relationshipExists) => {
+                if (relationshipExists) {
+                    getDemeritCount(to, from).then((currentCount) => {
+                        incrementDemeritCount(to, from, currentCount);
+                    });
+                } else {
+                    createRelationship(to, from);
+                }
+            }).catch((error) => {
+                console.log(error);
+                res.status(500).json({message: "An error occurred"});
+            });
+            
+            
+
+
+            res.status(201).json({ message: "Updated DB" });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Error occurred connecting to DB" });
+        }
     }
 });
 
 router.get('/demerits', function(req, res) {
     // Do graph db stuff
-    res.json({ message: "done" });
+    fetchData().then((data) => {
+        res.json(data);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).json({message: "An error occurred"});
+    });
 });
 
 app.use('/', router);
 
 app.listen(port);
 console.log('Api running on port ' + port);
+
+function userExists(user) {
+    return new Promise((resolve, reject) => {
+        db.cypher({
+            query: 'MATCH (n:Slacker) WHERE n.name = {username} RETURN n',
+            params: {
+                username: user,
+            }
+        }, function(err, results){
+            if (err) {
+                console.error('Error fetching user from DB:', err);
+                reject();
+            }
+            if (results.length > 0) {
+                console.log("Found user:", user);
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        });
+    })
+}
+
+function createUser(user) {
+    return new Promise((resolve, reject) => {
+        db.cypher({
+            query: 'CREATE (n:Slacker {name: {username}})',
+            params: {
+                username: user,
+            }
+        }, function(err, results){
+            if (err) {
+                console.error('Error creating new user:', err);
+                reject();
+            } else {
+                console.log('created new user:', user);
+                resolve();
+            }
+        });
+    });
+}
+
+function relationshipExists(to, from) {
+    return new Promise((resolve, reject) => {
+        db.cypher({
+            query: 'MATCH (n1:Slacker)-[:GAVE_DEMERIT]->(n2:Slacker) WHERE n1.name = {fromUser} AND n2.name = {toUser} RETURN n1, n2',
+            params: {
+                toUser: to,
+                fromUser: from
+            }
+        }, function(err, results){
+            if (err) {
+                console.error('Error looking for relationship:', err);
+                reject();
+            } else {
+                if (results.length > 0) {
+                    console.log("found relationship between:", from, to);
+                    resolve(true);
+                } else {
+                    console.log("No relationship between:", from, to);
+                    resolve(false);
+                }
+            }
+        });
+    });
+}
+
+function createRelationship(to, from) {
+    return new Promise((resolve, reject) => {
+        db.cypher({
+            query: 'MATCH (to:Slacker), (from:Slacker) WHERE to.name = {toUser} AND from.name = {fromUser} CREATE (from)-[:GAVE_DEMERIT {count: 1}]->(to)',
+            params: {
+                toUser: to,
+                fromUser: from
+            }
+        }, function(err, results){
+            if (err) {
+                console.error('Error creating relationship:', err);
+                reject();
+            } else {
+                console.log('created new relationship:', from, to);
+                resolve();
+            }
+        });
+    });
+}
+
+function getDemeritCount(to, from) {
+    return new Promise((resolve, reject) => {
+        db.cypher({
+            query: 'MATCH (n1:Slacker)-[r:GAVE_DEMERIT]->(n2:Slacker) WHERE n1.name = {fromUser} AND n2.name = {toUser} RETURN r.count',
+            params: {
+                toUser: to,
+                fromUser: from
+            }
+        }, function(err, results){
+            if (err) {
+                console.error('Error getting demerit count:', err);
+                reject();
+            } else {
+                const count = results[0]["r.count"];
+                console.log('Demerits:', from, to, count);
+                resolve(count);
+            }
+        });
+    });
+}
+
+function incrementDemeritCount(to, from, currentCount) {
+    return new Promise((resolve, reject) => {
+        const newCount = currentCount + 1;
+        db.cypher({
+            query: 'MATCH (from:Slacker)-[r:GAVE_DEMERIT]->(to:Slacker) WHERE to.name = {toUser} AND from.name = {fromUser} SET r.count = {count}',
+            params: {
+                toUser: to,
+                fromUser: from,
+                count: newCount
+            }
+        }, function(err, results){
+            if (err) {
+                console.error('Error incrementing demerit count:', err);
+                reject();
+            } else {
+                console.log('incremented demerit count:', from, to, newCount);
+                resolve();
+            }
+        });
+    });
+}
+
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        db.cypher({
+            query: 'MATCH (from:Slacker)-[action:GAVE_DEMERIT]->(to:Slacker) RETURN from, to, action'
+        }, function(err, results){
+            if (err) {
+                console.error('Error fetching all data:', err);
+                reject();
+            } else {
+                console.log('returning all data');
+                resolve(results);
+            }
+        });
+    });
+}
